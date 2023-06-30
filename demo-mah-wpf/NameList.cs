2aa8bfd184690eed3c198d28ddcd9c01b48e622f
Keith Yat Lam Poon <kepoon@deloitte.com.hk>
2023-06-30 17:07:46 +0800

    add testing listview, table

---
+++ demo-mah-wpf/NameList.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo_mah_wpf
+{
+    public class NameList : ObservableCollection<PersonObject>
+    {
+        public NameList() : base()
+        {
+            Add(new PersonObject("1", DateTime.Now, "Willa", "Cather"));
+            Add(new PersonObject("2", DateTime.Now, "Isak", "Dinesen"));
+            Add(new PersonObject("3", DateTime.Now, "Victor", "Hugo"));
+            Add(new PersonObject("4", DateTime.Now, "Jules", "Verne"));
+        }
+    }
+
+    
+}
---
+++ demo-mah-wpf/PersonObject.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo_mah_wpf
+{
+
+    public class PersonObject : INotifyPropertyChanged
+    {
+        private string firstName;
+        private string lastName;
+        private string id;
+        private DateTime date;
+        public string ID
+        {
+            get { return id; }
+            set { id = value;
+                // Call OnPropertyChanged whenever the property is updated
+                OnPropertyChanged();
+            }
+        }
+        public DateTime Date
+        {
+            get { return date; }
+            set { date = value; }
+        }
+
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+
+
+        // Declare the event
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public PersonObject()
+        {
+        }
+
+        public PersonObject(string id, DateTime date)
+        {
+            this.id = id;
+            this.date = date;
+        }
+        public PersonObject(string id, DateTime date, string firstName, string lastName)
+        {
+            this.id = id;
+            this.date = date;
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+
+        // Create the OnPropertyChanged method to raise the event
+        // The calling member's name will be used as the parameter.
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
