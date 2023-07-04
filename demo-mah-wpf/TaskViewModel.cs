67ee35c352d31406ed29a1c3054f95224624dd0c
Keith Yat Lam Poon <kepoon@deloitte.com.hk>
2023-07-04 11:46:50 +0800

    add dispatchertimer, generate dummy data on every 5 seconds

--- demo-mah-wpf/TaskViewModel.cs
+++ demo-mah-wpf/TaskViewModel.cs
@@ -9,65 +9,74 @@ using System.Collections.Specialized;
 using System.Windows.Data;
 using System.ComponentModel;
 using demo_mah_wpf.Entity;
+using System.Windows.Threading;
 
 namespace demo_mah_wpf
 {
-    public class TaskViewModel : BaseViewModel
+    public class TaskViewModel
     {
-        public event NotifyCollectionChangedEventHandler CollectionChanged;
-        private ObservableCollection<Task> _Tasks;
-        public ObservableCollection<Task> Tasks
-        {
-            get { return _Tasks; }
-            set
-            {
-                _Tasks = value;
-                SetProperty(ref _Tasks, value, () => Tasks);
-            }
-        }
+
+        public ObservableCollection<Task> TaskCollection { get; }
+            = new ObservableCollection<Task>();
+        private readonly DispatcherTimer timer = new DispatcherTimer();
+        private readonly Random random = new Random();
 
         private static object _lock = new object();
         public TaskViewModel() : base()
         {
-            //this.ctxFactory = ccf; //DB Context, using DI
-            this.Tasks = new ObservableCollection<Task>();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += TimerTick;
+            timer.Start();
 
+            //BindingOperations.EnableCollectionSynchronization(TaskCollection, _lock);
 
-            BindingOperations.EnableCollectionSynchronization(Tasks, _lock);
 
             // use async in .net framework 4.7.2
             // otherwise, complie error: async streams is not available in 7.3
             // https://bartwullems.blogspot.com/2020/01/asynchronous-streams-using.html
 
-            System.Threading.Tasks.Task.Run(async () => {
-                var getTaskResult = this.GetAllTasks();
-                if(getTaskResult != null && getTaskResult.Result != null)
-                {
-                    var getTaskResultList = getTaskResult.Result.ToList();
-                    if (getTaskResultList.Count != 0)
-                    {
-                        foreach (Task task in getTaskResultList)
-                        {
-                            //Add(new Task(task.TaskName, task.Description, task.Priority, task.TaskType));
-                            this.Tasks.Add(task);
-                            CollectionChanged?.Invoke(this, new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Add));
-                        }
-                        //Add(task);
+            //System.Threading.Tasks.Task.Run(async () => {
+            //    var getTaskResult = this.GetAllTasks();
+            //    if(getTaskResult != null && getTaskResult.Result != null)
+            //    {
+            //        var getTaskResultList = getTaskResult.Result.ToList();
+            //        if (getTaskResultList.Count != 0)
+            //        {
+            //            foreach (Task task in getTaskResultList)
+            //            {
+            //                //Add(new Task(task.TaskName, task.Description, task.Priority, task.TaskType));
+            //                this.Tasks.Add(task);
+            //                CollectionChanged?.Invoke(this, new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Add));
+            //            }
+            //            //Add(task);
 
-                    }
-                }
-            });
+            //        }
+            //    }
+            //});
         }
 
         private async System.Threading.Tasks.Task<IEnumerable<Task>> GetAllTasks()
         {
-            List<Task> Items = new List<Task>();
+            List<Task> TaskCollection = new List<Task>();
             for (int i = 1; i <= 3; i++)
             {
                 await System.Threading.Tasks.Task.Delay(1000);
-                Items.Add(new Task(string.Format("Development {0}",i+1), "Write a WPF program", 2, TaskType.Home));
+                TaskCollection.Add(new Task(string.Format("Development {0}",i+1), "Write a WPF program", 2, TaskType.Home));
             }
-            return Items;
+            return TaskCollection;
+        }
+        private async void TimerTick(object sender, EventArgs args)
+        {
+            this.TaskCollection.Clear();
+            await System.Threading.Tasks.Task.Delay(1000);
+            for (int i = 1; i <= 10; i++)
+            {
+                this.TaskCollection.Add(new Task(string.Format("Development {0}", i + 1), "Write a WPF program", 2, TaskType.Home));
+            }
+            //this.Tasks.Add(new Item { ItemData = $"Item Data: {random.Next(100)}" });
+            //this.Tasks.Add(new Item { ItemData = $"Item Data: {random.Next(100)}" });
+            //this.Tasks.Add(new Item { ItemData = $"Item Data: {random.Next(100)}" });
+            //this.Tasks.Add(new Item { ItemData = $"Item Data: {random.Next(100)}" });
         }
     }
 }
